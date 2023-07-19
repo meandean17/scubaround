7cc0aa88a04fa0f7ad98ed1c44fd1d20df020f54
meandean17 <deenomio@gmail.com>
2023-07-20 01:21:38 +0300

    search bar and search functionality in dive history

--- list.php
+++ list.php
@@ -20,7 +20,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <style>
+
         @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
     </style>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
@@ -112,21 +114,32 @@
         <div class="content-background">
             <div class="main-header">
                 <h2>Dive History</h2>
-                <form action="#" method="get">
-                    <input type="search" class="form-control search-bar">
+                <form action="#" method="get" class="search-form">
+                    <input type="search" class="form-control search-bar" placeholder="Search here..." name="keyword" required="required" value="<?php echo isset($_POST['keyword']) ? $_POST['keyword'] : '' ?>"/>
+                    <button class="btn btn-primary search-btn" name="search">&#x1F50E;&#xFE0E;</button>
                 </form>
             </div>
             <ul id="diveList" class="dive-list">
                 <?php
-                     $query = "SELECT dives.dive_name, dives.is_public, details.dive_date, details.dive_duration, dives.dive_description
-                     FROM tbl_226_dives AS dives
-                     INNER JOIN tbl_226_dive_details AS details ON dives.dive_id = details.dive_id
-                     WHERE dives.user_id =  '" . $_SESSION["user_id"] . "'";
-
+                    $keyword = "";
+                    if(ISSET($_GET['search']))
+                        $keyword = $_GET['keyword'];
+                    
+                    if($keyword)
+                    {
+                        $query = "SELECT dives.dive_name, dives.is_public, details.dive_date, details.dive_duration, dives.dive_description
+                        FROM tbl_226_dives AS dives
+                        INNER JOIN tbl_226_dive_details AS details ON dives.dive_id = details.dive_id
+                        WHERE dives.user_id =  '" . $_SESSION["user_id"] . "' AND (dives.dive_name LIKE '%" . $keyword . "%')";
+                    }
+                    else {
+                        $query = "SELECT dives.dive_name, dives.is_public, details.dive_date, details.dive_duration, dives.dive_description
+                        FROM tbl_226_dives AS dives
+                        INNER JOIN tbl_226_dive_details AS details ON dives.dive_id = details.dive_id
+                        WHERE dives.user_id =  '" . $_SESSION["user_id"] . "'";
+                    }
                     $result = mysqli_query($connection, $query);
-                    $row = mysqli_fetch_array($result);
-
-                    if(is_array($row))
+                    while ($row = mysqli_fetch_assoc($result))
                     {
                         $diveName = $row['dive_name'];
                         $diveDate = $row['dive_date'];
